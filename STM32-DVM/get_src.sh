0a68c4fb72b85403005af4eb2f36628d6e8c1e2b
Steve N4IRS <szingman@msgstor.com>
2017-06-23 14:12:54 +0100

    First draft STM32-DVM support

--- STM32-DVM/get_src.sh
+++ STM32-DVM/get_src.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
-cd /tmp
+cd /srv
 
 wget -O stm32cube_fw_f1_v140.zip https://sourceforge.net/projects/micro-os-plus/files/Vendor%20Archives/STM32/stm32cube_fw_f1_v140.zip/download
 
-cd /opt
-
-unzip /tmp/stm32cube_fw_f1_v140.zip
+unzip -d /opt stm32cube_fw_f1_v140.zip
 
 cd /srv/
-git clone
+git clone https://github.com/g4klx/MMDVM.git
+
+cp -rf /srv/MMDVM /usr/src
+
+cp Config.h /usr/src/MMDVM
+
+
+
+
 
--- STM32-DVM/required-libs.sh
+++ STM32-DVM/required-libs.sh
@@ -14,43 +14,8 @@ apt-get install -y libnewlib-arm-none-eabi
 apt-get install -y make
 apt-get install -y curl
 apt-get install -y python-pip
+apt-get install -y gcc-arm-none-eabi
 
-apt-get install stm32flash
+apt-get install -y stm32flash
 
-exit 0
-
-apt-get install libusb-dev -y
-apt-get install libnewt-dev -y
-apt-get install libeditline0 -y
-apt-get install libncurses5-dev -y
-
-apt-get install bison -y
-
-apt-get install libssl-dev -y
-
-apt-get install libasound2-dev -y
-
-apt-get install libcurl4-gnutls-dev -y
-
-apt-get install php5-cli -y
-
-apt-get install libiksemel-dev -y
-
-apt-get install libvorbis-dev -y
-
-apt-get install curl -y
-
-# Nice to have utilities and tools
-# is sox required or nice to have
-apt-get install sox -y
-apt-get install usbutils -y
-apt-get install alsa-utils -y
-apt-get install bc -y
-apt-get install dnsutils -y
-
-apt-get install g++ -y
-apt-get install make -y
-apt-get install build-essential -y
-
-apt-get install git -y
 
