baab8fea5b0e63ab13d5e19676477eac13985913
Steve Zingman <N4IRS@users.noreply.github.com>
2017-09-27 13:05:54 -0400

    Update get-src.sh

--- STM32-DVM/get-src.sh
+++ STM32-DVM/get-src.sh
@@ -2,7 +2,7 @@
 
 cd /srv
 
-wget -O stm32cube_fw_f1_v140.zip https://sourceforge.net/projects/micro-os-plus/files/Vendor%20Archives/STM32/stm32cube_fw_f1_v140.zip/download
+wget -O stm32cube_fw_f1_v140.zip http://dvswitch.org/files/HAM/MMDVM/stm32cube_fw_f1_v140.zip
 
 unzip -d /opt stm32cube_fw_f1_v140.zip
 
@@ -10,6 +10,10 @@ cd /srv/
 git clone https://github.com/g4klx/MMDVM.git
 
 cp -rf /srv/MMDVM /usr/src
+
+cd /usr/src/MMDVM
+
+git clone https://github.com/juribeparada/STM32F4XX_Lib.git
 
 cp /srv/MMDVM-Install/STM32-DVM/Config.h /usr/src/MMDVM
 
