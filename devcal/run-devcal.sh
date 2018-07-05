3584de530565354f1e03675b1709b4a39acf1f02
Steve N4IRS <szingman@msgstor.com>
2018-07-05 13:34:42 +0000

    Add info for direct connect RTL

--- devcal/run-devcal.sh
+++ devcal/run-devcal.sh
@@ -1,3 +1,5 @@
-# rtl_tcp should be running on localhost (or another machine)
-./devcal -M -f 1200 ./test.conf APRS
+# Can be used with USB connected RTL
+# rtl_tcp can be running on localhost (or another machine)
+# See comment in devcal.conf
+./devcal -M -f 1200 ./devcal.conf DEVCAL
 
